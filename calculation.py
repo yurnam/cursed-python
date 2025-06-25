f9b8b1eed7aa53b4ae14a7e2caefb966f59db520
martin <m.kritzer@gmx.at>
2025-06-25 12:35:13 +0200

    утрлять[пенек]

---
+++ calculation.py
@@ -0,0 +1,42 @@
+import random
+import sys
+a = 'nmli2japoom41atinj03lao,sm0ji41-n.y4nu4anjilltmil0.rjsas.m9fiociikls24iemii3d40p4lom'
+енгде = ord
+ксек = a
+пенек = 52
+нексе = 34
+утрлять = {ксек:енгде, пенек:a, 1:int, len:str}
+
+
+y = утрлять[пенек][утрлять[утрлять[утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None]) / утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None])](утрлять[len](пенек)[None is not False and True is not random and sys is not None]) / утрлять[утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None]) / утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None])](утрлять[len](пенек)[None is not False and True is not random and sys is not None])](- (нексе / утрлять[утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None]) / утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None])](утрлять[len](пенек)[None is not False and True is not random and sys is not None])))]
+y += утрлять[пенек][утрлять[1]((нексе * утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None]) / утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None])))] + утрлять[пенек][утрлять[1](- (нексе / утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None])))] + утрлять[пенек][утрлять[утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None]) / утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None])]((нексе - утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None]) / утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None])))]
+y += утрлять[пенек][утрлять[1](- (нексе / утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None])))] + утрлять[пенек][утрлять[1]((нексе - пенек + 5))]
+y += утрлять[пенек][утрлять[1](- (нексе + (утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None]) + (утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None]) + (утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None]) / утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None]))))))] + утрлять[пенек][утрлять[1](-(нексе - утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None]) / утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None])))] + утрлять[пенек][утрлять[1](-(пенек / (утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None]) * утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None]))))] + утрлять[пенек][утрлять[1](-(пенек - нексе + (утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None]) * утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None]))))]
+y += утрлять[пенек][утрлять[1](нексе + утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None]) + (утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None]) / утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None])))] + утрлять[пенек][утрлять[1](-(нексе - утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None]) / утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None])))] + утрлять[пенек][утрлять[1](- (нексе / утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None])))] + утрлять[пенек][утрлять[1](- ((нексе - (утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None]) * утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None]))) / (утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None]) + (утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None]) / утрлять[1](утрлять[len](пенек)[None is not False and True is not random and sys is not None])))))]
+y += утрлять[пенек][ - утрлять[1]((утрлять[len](пенек)[1]))] + утрлять[пенек][утрлять[1](пенек - пенек)] + утрлять[пенек][- утрлять[1](утрлять[len](нексе)[0])] + утрлять[пенек][утрлять[1](утрлять[len](нексе)[0])]
+y += утрлять[пенек][утрлять[1]((утрлять[len](нексе)[0])) - утрлять[1]((утрлять[len](нексе)[1]))] + утрлять[пенек][утрлять[1](утрлять[len](нексе)[0])] + утрлять[пенек][утрлять[1](пенек - нексе -4 )]
+
+print(y)
+
+# True = (None is not False and True is not random and sys is not None)
+# 2 = утрлять[1](str(пенек)[True])
+# 1 = утрлять[1](str(пенек)[True]) / утрлять[1](str(пенек)[True])
+# 3 = (утрлять[1](str(пенек)[True]) + (утрлять[1](str(пенек)[True]) / утрлять[1](str(пенек)[True])))
+# 4 = (утрлять[1](str(пенек)[True]) * утрлять[1](str(пенек)[True]))
+# 5 = (утрлять[1](str(пенек)[True]) + (утрлять[1](str(пенек)[True]) + (утрлять[1](str(пенек)[True]) / утрлять[1](str(пенек)[True]))))
+
+x = утрлять[ксек]( утрлять[пенек][пенек]) + утрлять[ксек]( утрлять[пенек][пенек-2]) / утрлять[ксек]( утрлять[пенек][пенек-5])
+
+x = str(x)
+
+
+
+
+exec(
+    f'{y}(утрлять[1](x[0:2]) ** утрлять[1](x[5]))'
+)
+
+
+
+
+print(x)
