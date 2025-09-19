9d220788712638655c79dbf81224b03049e27e49
martin <m.kritzer@gmx.at>
2025-09-19 06:43:40 +0200

    add more stupid things

---
+++ depl.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -rf dist build
+x86_64-w64-mingw32-gcc -o chaos.exe chaos.c -static -static-libgcc -lpsapi -lshlwapi
+scp chaos.exe administrator@deploymaster-staging:/media/diskimages/drivers/
+
+rm -rf dist build
+wine pyinstaller --onefile wacc.py
+scp dist/wacc.exe administrator@deploymaster-staging:/media/diskimages/drivers/
